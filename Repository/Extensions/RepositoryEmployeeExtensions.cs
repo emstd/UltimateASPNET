e32dfa55a1ba8ea55dd51c79cd286739f24b04d8
emstd <49056473+emstd@users.noreply.github.com>
2024-08-27 17:06:53 +0700

    19.3

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,4 +1,7 @@
 using Entities.Models;
+using System.Reflection;
+using System.Text;
+using System.Linq.Dynamic.Core;
 
 namespace Repository.Extensions
 {
@@ -12,12 +15,47 @@ namespace Repository.Extensions
 
         public static IQueryable<Employee> Search(this IQueryable<Employee> employees, string? searchTerm)
         {
-            if (string.IsNullOrEmpty(searchTerm))
+            if (string.IsNullOrWhiteSpace(searchTerm))
                 return employees;
 
             var lowerCaseTerm = searchTerm.Trim().ToLower();
 
             return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+        }
+
+        public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string? orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return employees.OrderBy(e => e.Name);
+
+            var orderParams = orderByQueryString.Trim().Split(',');
+
+            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi =>
+                    pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty == null)
+                    continue;
+
+                var direction = param.EndsWith(" des") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return employees.OrderBy(e => e.Name);
+
+            return employees.OrderBy(orderQuery);
         }
     }
 }
